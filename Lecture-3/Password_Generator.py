4fe852169aabb59048b37e050d281adec474c885
deanberrie <deanberrie@gmail.com>
2025-02-19 11:56:11 +1000

    Remove spaces from being generated into password

--- Lecture-3/Password_Generator.py
+++ Lecture-3/Password_Generator.py
@@ -8,10 +8,13 @@ def main():
     password_length = random.randint(8, 20)
     for character in range(password_length):
         character = random.choice(characters)
+        while character == " ":
+            character = random.choice(characters)
         password.append(character)
     final_password = (''.join(password))
     print(f'your final password is {final_password}')
+
     with open('password.txt', 'w') as out_file:
-        print(password, file=out_file)
+        print(final_password, file=out_file)
 
 main()
